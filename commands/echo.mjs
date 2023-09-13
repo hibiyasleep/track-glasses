7522540f6a31bd9b7c22f1f91b29e69e21c01b3e
Kuriyama Hibiya <kuriyama@hibiya.moe>
2023-09-13 20:36:00 +0900

    echo: raw

--- commands/echo.mjs
+++ commands/echo.mjs
@@ -1,10 +1,19 @@
 export default [{
   name: 'echo',
   type: 'chat',
-  test: (m, { config }) => 
+  test: (m, { config }) =>
 	 config.admin.includes(m.source?.nick)
   && /^!echo /.test(m.content),
   run(m, { chats }) {
     chats.send(`PRIVMSG ${m.params} :${m.content.slice(6)}`)
+  }
+}, {
+  name: 'raw',
+  type: 'chat',
+  test: (m, { config }) =>
+	 config.admin.includes(m.source?.nick)
+  && /^!raw /.test(m.content),
+  run(m, { chats }) {
+    chats.send(m.content.slice(5))
   }
 }]
